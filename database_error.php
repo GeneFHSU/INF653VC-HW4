3d3319cd2d5725e66b2a2decb7d4a11e948c2e69
GeneFHSU <78402723+GeneFHSU@users.noreply.github.com>
2021-02-15 21:36:04 -0800

    Update page title and header to ToDo List

--- database_error.php
+++ database_error.php
@@ -4,13 +4,13 @@
 
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>ToDo List</title>
     <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>My Guitar Shop</h1></header>
+<header><h1>ToDo List</h1></header>
 
 <main>
     <h1>Database Error</h1>
